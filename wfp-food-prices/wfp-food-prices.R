50f27691c4e29a08958fba056532b2527e7f5d54
Pauline Wairimu <paunimo@gmail.com>
2024-01-24 23:56:35 +0300

    adding questions

--- wfp-food-prices/wfp-food-prices.R
+++ wfp-food-prices/wfp-food-prices.R
@@ -17,13 +17,14 @@ get_rhdx_config()
 wfp_glb <- read_csv("https://data.humdata.org/dataset/31579af5-3895-4002-9ee3-c50857480785/resource/0f2ef8c4-353f-4af1-af97-9e48562ad5b1/download/wfp_countries_global.csv")
 
 # searching for wfp food price datasets on HDX and reading in resources
-wfp_fp_datasets <- search_datasets(" - Food Prices", rows = 30) %>%
+wfp_fp_datasets <- search_datasets(" - Food Prices", rows = 50) %>%
   purrr::set_names(map(., ~.x$data$title)) %>%
   subset(names(.) != "Global - Food Prices") %>%
   map(~.x %>% 
         get_resource(1) %>%
         read_resource()) 
 
+# removing possible duplicates
 wfp_fp_resources <- wfp_fp_datasets %>%
   names() %>%
   duplicated() %>%
@@ -31,4 +32,8 @@ wfp_fp_resources <- wfp_fp_datasets %>%
   wfp_fp_datasets[.]
 
 # exploring possible thresholds
-
+# Q1: Do different countries tend to have lower prices than others?
+# Q2: Pricetype: Wholesale vs retail
+# Q3: Which category/commodity is the most expensive?
+# Q4: 
+# Q5: 
