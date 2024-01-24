762d2c15e587613c2faafc1c754ecae7cfb065b3
Pauline Wairimu <paunimo@gmail.com>
2024-01-24 23:47:51 +0300

    Create wfp-food-prices.R

---
+++ wfp-food-prices/wfp-food-prices.R
@@ -0,0 +1,34 @@
+# libraries
+
+# for installing rhdx
+# install.packages("remotes")
+# remotes::install_gitlab("dickoa/rhdx")
+
+library(tidyverse)
+library(gghdx)
+library(rhdx)
+gghdx()
+
+# rhdx config
+set_rhdx_config(hdx_site = "prod")
+get_rhdx_config()
+
+# reading in the global dataset
+wfp_glb <- read_csv("https://data.humdata.org/dataset/31579af5-3895-4002-9ee3-c50857480785/resource/0f2ef8c4-353f-4af1-af97-9e48562ad5b1/download/wfp_countries_global.csv")
+
+# searching for wfp food price datasets on HDX and reading in resources
+wfp_fp_datasets <- search_datasets(" - Food Prices", rows = 30) %>%
+  purrr::set_names(map(., ~.x$data$title)) %>%
+  subset(names(.) != "Global - Food Prices") %>%
+  map(~.x %>% 
+        get_resource(1) %>%
+        read_resource()) 
+
+wfp_fp_resources <- wfp_fp_datasets %>%
+  names() %>%
+  duplicated() %>%
+  `!`() %>%
+  wfp_fp_datasets[.]
+
+# exploring possible thresholds
+
